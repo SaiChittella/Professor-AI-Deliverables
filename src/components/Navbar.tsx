5dad5b27b8ebbf950baa882488d1f66e65574d9b
SaiChittella <95110779+SaiChittella@users.noreply.github.com>
2024-05-06 12:35:07 -0700

    Add files via upload
    added circle to indicate what tag the user has clicked on in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,80 @@
+"use client";
+import { useState } from "react";
 interface Props {
 	sendDataToParent: (data: string) => void;
 }
 const Navbar: React.FC<Props> = ({ sendDataToParent }) => {
+	const [activeLink, setActiveLink] = useState("overview");
+
 	const handleClick = (data: string) => {
+		setActiveLink(data);
 		sendDataToParent(data);
 	};
 
 	return (
 		<nav className="mt-6 hidden w-fit items-center space-x-2 rounded-full border border-gray-200 bg-white p-2 sm:flex transition-all hover:shadow-lg cursor-pointer hover:cursor-pointer">
 			<a
-				className="relative z-10"
+				className={`relative z-10 ${
+					activeLink === "overview" ? "text-white" : ""
+				}`}
 				onClick={() => handleClick("overview")}
 			>
-				<div className="rounded-full px-4 py-2 text-sm font-medium transition-all text-white">
+				<div className="rounded-full px-4 py-2 text-sm font-mediumtransition-all hover:text-gray-500">
 					Overview
 				</div>
-
-				<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				{activeLink === "overview" && (
+					<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				)}
 			</a>
 			<a
-				className="relative z-10"
+				className={`relative z-10 ${
+					activeLink === "Company News" ? "text-white" : ""
+				}`}
 				onClick={() => handleClick("Company News")}
 			>
-				<div className="rounded-full px-4 py-2 text-sm font-medium text-gray-800 transition-all hover:text-gray-500">
+				{activeLink === "Company News" && (
+					<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				)}
+				<div className="rounded-full px-4 py-2 text-sm font-mediumtransition-all hover:text-gray-500">
 					Company News
 				</div>
 			</a>
 			<a
-				className="relative z-10"
+				className={`relative z-10 ${
+					activeLink === "Education" ? "text-white" : ""
+				}`}
 				onClick={() => handleClick("Education")}
 			>
-				<div className="rounded-full px-4 py-2 text-sm font-medium text-gray-800 transition-all hover:text-gray-500">
+				{activeLink === "Education" && (
+					<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				)}
+				<div className="rounded-full px-4 py-2 text-sm font-mediumtransition-all hover:text-gray-500">
 					Education
 				</div>
 			</a>
 			<a
-				className="relative z-10"
+				className={`relative z-10 ${
+					activeLink === "Engineering" ? "text-white" : ""
+				}`}
 				onClick={() => handleClick("Engineering")}
 			>
-				<div className="rounded-full px-4 py-2 text-sm font-medium text-gray-800 transition-all hover:text-gray-500">
+				{activeLink === "Engineering" && (
+					<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				)}
+				<div className="rounded-full px-4 py-2 text-sm font-medium transition-all hover:text-gray-500">
 					Engineering
 				</div>
 			</a>
 			<a
-				className="relative z-10"
+				className={`relative z-10 ${
+					activeLink === "Customer Stories" ? "text-white" : ""
+				}`}
 				onClick={() => handleClick("Customer Stories")}
 			>
-				<div className="rounded-full px-4 py-2 text-sm font-medium text-gray-800 transition-all hover:text-gray-500">
+				{activeLink === "Customer Stories" && (
+					<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
+				)}
+				<div className="rounded-full px-4 py-2 text-sm font-medium transition-all hover:text-gray-500">
 					Customer Stories
 				</div>
 			</a>
