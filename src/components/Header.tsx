28d17bb8fe5365185f1bbc79a1e2c2080fa4174e
SaiChittella <95110779+SaiChittella@users.noreply.github.com>
2024-05-05 21:36:48 -0700

    Add files via upload
    Final changes + SEO Optimizing with meta tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import { MouseEventHandler, useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import Head from "next/head";
 
 interface BlogPostFields {
 	title: string;
@@ -24,7 +25,6 @@ const client = createClient({
 
 export default function Header() {
 	const [blogPosts, setBlogPosts] = useState<BlogPostEntry[]>([]);
-	const router = useRouter();
 	const [dataFromChild, setDataFromChild] = useState<string>("overview");
 
 	const handleDataFromChild = (data: string) => {
@@ -47,72 +47,76 @@ export default function Header() {
 	}, []);
 
 	return (
-		<div className="relative z-10">
-			<div className="flex flex-col min-h-screen justify-between">
-				<div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20">
-					<div className="max-w-screen-md py-16">
-						<h1 className="font-display text-3xl font-extrabold text-gray-700 sm:text-4xl">
-							Blog
-						</h1>
-						<p className="mt-4 text-xl text-gray-500">
-							Latest news and updates from ProfessorAI.co
-						</p>
-						<Navbar sendDataToParent={handleDataFromChild} />
+		<div>
+			<Head>
+				<title>Blog Post</title>
+				<meta name="Contains an example of a blog post using a headless CMS (Contentful) to pull blog posts. Uses NextJS 14 and TailwindCSS for styling, with typeScript." />
+			</Head>
+			<div className="relative z-10">
+				<div className="flex flex-col min-h-screen justify-between">
+					<div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20">
+						<div className="max-w-screen-md py-16">
+							<h1 className="font-display text-3xl font-extrabold text-gray-700 sm:text-4xl">
+								Blog
+							</h1>
+							<p className="mt-4 text-xl text-gray-500">
+								Latest news and updates from ProfessorAI.co
+							</p>
+							<Navbar sendDataToParent={handleDataFromChild} />
+						</div>
 					</div>
-				</div>
-				<div className="min-h-[100vh] w-full border-t border-gray-200 bg-gradient-to-b from-white/50 to-transparent backdrop-blur-lg">
-					<div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 grid grid-cols-1 gap-4 py-10 md:grid-cols-3 sm:grid-cols-2">
-						{blogPosts
-							.filter(
-								(post) =>
-									post.fields.tag === dataFromChild ||
-									dataFromChild === "overview"
-							)
-							.map((post) => (
-								<Link
-									href={`${post.fields.slug}`}
-									key={post.sys.id}
-									className="flex flex-col rounded-lg border border-gray-300 transition-all hover:shadow-lg cursor-pointer hover:cursor-pointer"
-								>
-									<img
-										className="object-cover w-full h-1/2 rounded-lg"
-										src={
-											post.fields.coverImage.fields.file
-												.url
-										}
-									/>
-									<div className="flex flex-1 flex-col justify-between rounded-b-lg bg-white p-6">
-										<div>
-											<p className="line-clamp-2 font-display text-xl font-bold text-gray-700">
-												{post.fields.title}
-											</p>
-											<p className="mt-2 line-clamp-2 text-sm text-gray-500">
-												{post.fields.excerpt}
-											</p>
-										</div>
-										<div className="mt-4 flex items-center space-x-2">
-											<div className="flex items-center -space-x-2">
-												<img
-													className="blur-0 rounded-full transition-all group-hover:brightness-90"
-													src={
-														post.fields.author
-															.fields.file.url
-													}
-													alt=""
-													width="36"
-													height="36"
-												/>
+					<div className="min-h-[100vh] w-full border-t border-gray-200 bg-gradient-to-b from-white/50 to-transparent backdrop-blur-lg">
+						<div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 grid grid-cols-1 gap-4 py-10 md:grid-cols-3 sm:grid-cols-2">
+							{blogPosts
+								.filter(
+									(post) =>
+										post.fields.tag === dataFromChild ||
+										dataFromChild === "overview"
+								)
+								.map((post) => (
+									<Link
+										href={`${post.fields.slug}`}
+										key={post.sys.id}
+										className="flex flex-col rounded-lg border border-gray-300 transition-all hover:shadow-lg cursor-pointer hover:cursor-pointer"
+									>
+										<img
+											className="object-cover w-full h-1/2 rounded-lg"
+											src={
+												post.fields.coverImage.fields
+													.file.url
+											}
+										/>
+										<div className="flex flex-1 flex-col justify-between rounded-b-lg bg-white p-6">
+											<div>
+												<p className="line-clamp-2 font-display text-xl font-bold text-gray-700">
+													{post.fields.title}
+												</p>
+												<p className="mt-2 line-clamp-2 text-sm text-gray-500">
+													{post.fields.excerpt}
+												</p>
 											</div>
-											<p>{post.fields.date}</p>
+											<div className="mt-4 flex items-center space-x-2">
+												<div className="flex items-center -space-x-2">
+													<img
+														className="blur-0 rounded-full transition-all group-hover:brightness-90"
+														src={
+															post.fields.author
+																.fields.file.url
+														}
+														alt=""
+														width="36"
+														height="36"
+													/>
+												</div>
+												<p>{post.fields.date}</p>
+											</div>
 										</div>
-									</div>
-								</Link>
-							))}
+									</Link>
+								))}
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
-
-// sunday May 5: get the sorting thing done -> just add a "tag" for each of the blog posts, and then render them or smth like that
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC<Props> = ({ sendDataToParent }) => {
 	};
 
 	return (
-		<nav className="mt-6 hidden w-fit items-center space-x-2 rounded-full border border-gray-200 bg-white p-2 sm:flex">
+		<nav className="mt-6 hidden w-fit items-center space-x-2 rounded-full border border-gray-200 bg-white p-2 sm:flex transition-all hover:shadow-lg cursor-pointer hover:cursor-pointer">
 			<a
 				className="relative z-10"
 				onClick={() => handleClick("overview")}
@@ -15,6 +15,7 @@ const Navbar: React.FC<Props> = ({ sendDataToParent }) => {
 				<div className="rounded-full px-4 py-2 text-sm font-medium transition-all text-white">
 					Overview
 				</div>
+
 				<div className="absolute left-0 top-0 h-full w-full rounded-full bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-800 z-[-1] opacity-1"></div>
 			</a>
 			<a
